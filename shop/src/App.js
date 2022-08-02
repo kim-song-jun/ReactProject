3704e7e5b6cf2a7547f0daa072ff7b6ae30aa73b
kimsongjun <kinsongjun13@gmail.com>
2022-08-02 17:06:33 +0900

    [Part 2-3] 0802 코드 길어지면 import export 하면 됩니다

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,55 +1,54 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import './App.css';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./App.css";
+import data from "./data.js";
+import { useState } from "react";
 
 function App() {
+  let [shoes, setShoes] = useState(data);
+
   return (
-    <div className='App'>
-      <Navbar bg='dark' variant='dark'>
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href='#home'>ShoeShop</Navbar.Brand>
-          <Nav className='me-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#features'>Cart</Nav.Link>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className='main-bg'></div>
+      <div className="main-bg"></div>
       <Container>
         <Row>
-          <Col md={4}>
-            {/* <img src={process.env.PUBLIC_URL + '/logo192.png'} /> */}
-            <img
-              src='https://codingapple1.github.io/shop/shoes1.jpg'
-              width='80%'
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-
-          <Col md={4}>
-            <img
-              src='https://codingapple1.github.io/shop/shoes2.jpg'
-              width='80%'
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-
-          <Col md={4}>
-            <img
-              src='https://codingapple1.github.io/shop/shoes3.jpg'
-              width='80%'
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
+          {shoes.map(function (e, i) {
+            return <Shoe shoe={e}></Shoe>;
+          })}
         </Row>
       </Container>
     </div>
+  );
+}
+
+function Shoe(props) {
+  return (
+    <>
+      <Col md={4}>
+        <img
+          src={
+            "https://codingapple1.github.io/shop/shoes" +
+            (props.shoe.id + 1) +
+            ".jpg"
+          }
+          width="80%"
+        />
+        <h4>{props.shoe.title}</h4>
+        <p>{props.shoe.content}</p>
+      </Col>
+    </>
   );
 }
 
