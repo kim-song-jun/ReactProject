d4eed3f2b6037ab72e59e463569e1edeefa43688
kimsongjun <kinsongjun13@gmail.com>
2022-08-06 06:19:25 +0900

    [Part 2-9] 0806 Lifecycle과 useEffenct 1

--- shop/src/components/Detail.js
+++ shop/src/components/Detail.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/esm/Col';
 import Container from 'react-bootstrap/esm/Container';
@@ -11,16 +12,46 @@ let ColorBtn = styled.button`
   padding: 10px;
 `;
 
+// 컴포넌트에 갈고리 다는법 (옛날)
+// class Detail2 extends React.Component{
+//   componentDidMount() {
+//     // mount 될때
+//   }
+//   componentDidUpdate() {
+//     // update 될 때
+//   }
+//   componentWillUnmount() {
+//     // unmout 될 때
+//   }
+// }
+
 function Detail(props) {
+  // 컴포넌트에 갈고리 다는법 (요즘)
+  // 여기 안에 적힌 코드는 mount, update시 실행됨
+  useEffect(() => {
+    // 실행 시점이 다름
+    // html 렌더링 후에 작동함
+    // 어려운 동작을 렌더링 한 후에 계산할 수 있음(ex 반복문 10000번)
+    // 안에 적는 코드들:
+    //  - 어려운 연산
+    //  - 서버에서 데이터 가져오는 작업
+    //  - 타이머 장착하는거
+    setTimeout(() => {
+      setDiscount(false);
+    }, 2000);
+    console.log('hello');
+  });
+  let [discount, setDiscount] = useState(true);
   let { id } = useParams();
-
   let shoe = props.shoes.find((x) => x.id == id);
-
   return (
     <Container>
+      {discount == true ? (
+        <div className='alert alert-warning'>2초 이내 구매시 할인</div>
+      ) : null}
       <Row>
-        <ColorBtn bg='orange'>버튼</ColorBtn>
-        <ColorBtn bg='blue'>버튼</ColorBtn>
+        {/* <ColorBtn bg='orange'>버튼</ColorBtn>
+        <ColorBtn bg='blue'>버튼</ColorBtn> */}
         <Col md={4}>
           <img
             src={
